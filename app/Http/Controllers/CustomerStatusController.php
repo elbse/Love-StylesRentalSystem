da37d9da54d18fad135256270f14b1383efd0d74
loftyyyy <lofty08.business@gmail.com>
2025-09-25 16:07:19 +0800

    Added separate status tables

---
+++ app/Http/Controllers/CustomerStatusController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CustomerStatus;
+use Illuminate\Http\Request;
+
+class CustomerStatusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CustomerStatus $customerStatus)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CustomerStatus $customerStatus)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CustomerStatus $customerStatus)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CustomerStatus $customerStatus)
+    {
+        //
+    }
+}
