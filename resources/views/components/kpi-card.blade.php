8e5e7e99941bfde5426351164757291468200e10
elbse <changpriego@gmail.com>
2025-09-28 12:57:49 +0800

    feat: updated kpi-card and added create folder

---
+++ resources/views/components/kpi-card.blade.php
@@ -0,0 +1,22 @@
+<div class="relative w-80 h-40 rounded-2xl shadow-lg p-5 overflow-hidden {{ $background }}">
+
+    <!-- Background symbol -->
+    <div class="absolute inset-0 flex items-center justify-center opacity-50 text-white text-[20rem] font-extrabold select-none">
+        @if(Str::endsWith($symbol, ['.png', '.jpg', '.jpeg', '.svg', '.gif']))
+            <img src="{{ asset($symbol) }}" alt="symbol" class="w-80 h-40 object-contain opacity-50">
+        @else
+            {{ $symbol ?? '₱' }}
+        @endif
+    </div>
+
+    <!-- Top right icon -->
+    <div class="absolute top-4 right-4 {{ $color }} rounded-full w-12 h-12 flex items-center justify-center shadow-md">
+        <i class="{{ $icon }} text-white text-xl"></i>
+    </div>
+
+    <div class="relative text-white">
+        {{ $slot }}
+    </div>
+</div>
+
+{{-- color="bg-gradient-to-r from-[#A4B1FF] to-[#5E72E4]" --}}
--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -1,5 +1,24 @@
 <x-layout>
 
+<form>
+    <h2> Create Customer </h2>
 
+    <label for="name">Customer Name:    </label>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</form>
 
 </x-layout>
