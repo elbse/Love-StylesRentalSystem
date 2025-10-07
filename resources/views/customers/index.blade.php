3539b96e5ee8eb072f3ccc6d5511784cf0bd3030
loftyyyy <lofty08.business@gmail.com>
2025-10-07 23:24:14 +0800

    Made the plain create customer better

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -284,13 +284,6 @@
             <div class="absolute inset-0 opacity-10">
                 <div class="absolute inset-0" style="background-image: radial-gradient(circle at 20% 20%, white 2px, transparent 2px), radial-gradient(circle at 80% 80%, white 2px, transparent 2px); background-size: 30px 30px;"></div>
             </div>
-            
-            <!-- Background Icon -->
-            <div class="absolute top-2 right-2 opacity-20">
-                <svg class="w-16 h-16 text-white" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z"/>
-                </svg>
-            </div>
 
             <!-- Content -->
             <div class="relative z-10">
@@ -312,14 +305,6 @@
             <!-- Enhanced Background Pattern -->
             <div class="absolute inset-0 opacity-10">
                 <div class="absolute inset-0" style="background-image: radial-gradient(circle at 20% 20%, white 2px, transparent 2px), radial-gradient(circle at 80% 80%, white 2px, transparent 2px); background-size: 30px 30px;"></div>
-            </div>
-            
-            <!-- Background Icon -->
-            <div class="absolute top-2 right-2 opacity-20">
-                <svg class="w-16 h-16 text-white" fill="currentColor" viewBox="0 0 20 20">
-                    <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"/>
-                    <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"/>
-                </svg>
             </div>
 
             <!-- Content -->
@@ -344,13 +329,6 @@
             <div class="absolute inset-0 opacity-10">
                 <div class="absolute inset-0" style="background-image: radial-gradient(circle at 20% 20%, white 2px, transparent 2px), radial-gradient(circle at 80% 80%, white 2px, transparent 2px); background-size: 30px 30px;"></div>
             </div>
-            
-            <!-- Background Icon -->
-            <div class="absolute top-2 right-2 opacity-20">
-                <svg class="w-16 h-16 text-white" fill="currentColor" viewBox="0 0 20 20">
-                    <path fill-rule="evenodd" d="M3 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"/>
-                </svg>
-            </div>
 
             <!-- Content -->
             <div class="relative z-10">
@@ -372,13 +350,6 @@
             <!-- Enhanced Background Pattern -->
             <div class="absolute inset-0 opacity-10">
                 <div class="absolute inset-0" style="background-image: radial-gradient(circle at 20% 20%, white 2px, transparent 2px), radial-gradient(circle at 80% 80%, white 2px, transparent 2px); background-size: 30px 30px;"></div>
-            </div>
-            
-            <!-- Background Icon -->
-            <div class="absolute top-2 right-2 opacity-20">
-                <svg class="w-16 h-16 text-white" fill="currentColor" viewBox="0 0 20 20">
-                    <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd"/>
-                </svg>
             </div>
 
             <!-- Content -->
@@ -471,7 +442,7 @@
     
 
     <!-- Table -->
-    <div class="overflow-x-auto">
+    <div class="overflow-x-auto" style="background-image: url('{{ asset('storage/images/Vector_34.png') }}'); background-size: 920px 620px; background-repeat: no-repeat; background-position: center;">
         <table class="min-w-full border-collapse">
             <thead>
                 <tr class="bg-purple-100 text-gray-700">
