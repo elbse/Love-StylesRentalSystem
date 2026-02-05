bad541e23618a17177f54bb4624a4047bfa91e8e
elbse <changpriego@gmail.com>
2026-02-05 14:21:50 +0800

    dashboard update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,52 @@
 <x-layout>
 
-    
+    <h1 class="text-2xl font-bold text-gray-800 mb-6">Key Performance Indicators</h1>
+
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-6">
+        <x-kpi-card icon="fas fa-users" color="bg-black" symbol="{{ asset('storage/images/vector_peso.png') }}"  background="bg-white">
+                <h3 class="text-sm text-black">Total Customers</h3>
+            </x-kpi-card>
+
+        <x-kpi-card icon="fas fa-users" color="bg-black" symbol="{{ asset('storage/images/vector_peso.png') }}"  background="bg-white">
+                <h3 class="text-sm text-black">Total Customers</h3>
+            </x-kpi-card>
+
+        <x-kpi-card icon="fas fa-users" color="bg-black" symbol="{{ asset('storage/images/vector_peso.png') }}"  background="bg-white">
+                <h3 class="text-sm text-black">Total Customers</h3>
+            </x-kpi-card>
+
+        <x-kpi-card icon="fas fa-users" color="bg-black" symbol="{{ asset('storage/images/vector_peso.png') }}"  background="bg-white">
+                <h3 class="text-sm text-black">Total Customers</h3>
+            </x-kpi-card>
+    </div>
+
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-4 mb-6">
+
+        <x-kpi-card icon="fas fa-users" color="bg-black" symbol="{{ asset('storage/images/vector_peso.png') }}"  background="bg-white">
+                <h3 class="text-sm text-black">Total Customers</h3>
+            </x-kpi-card>
+
+        <x-kpi-card icon="fas fa-users" color="bg-black" symbol="{{ asset('storage/images/vector_peso.png') }}"  background="bg-white">
+                <h3 class="text-sm text-black">Total Customers</h3>
+            </x-kpi-card>
+
+         <x-kpi-card icon="fas fa-users" color="bg-black" symbol="{{ asset('storage/images/vector_peso.png') }}"  background="bg-white">
+                <h3 class="text-sm text-black">Total Customers</h3>
+            </x-kpi-card>
+
+        <x-kpi-card icon="fas fa-users" color="bg-black" symbol="{{ asset('storage/images/vector_peso.png') }}"  background="bg-white">
+                <h3 class="text-sm text-black">Total Customers</h3>
+            </x-kpi-card>
+
+         <x-kpi-card icon="fas fa-users" color="bg-black" symbol="{{ asset('storage/images/vector_peso.png') }}"  background="bg-white">
+                <h3 class="text-sm text-black">Total Customers</h3>
+            </x-kpi-card>
+
+        <x-kpi-card icon="fas fa-users" color="bg-black" symbol="{{ asset('storage/images/vector_peso.png') }}"  background="bg-white">
+                <h3 class="text-sm text-black">Total Customers</h3>
+            </x-kpi-card>
+
+    </div>
     
    
 
