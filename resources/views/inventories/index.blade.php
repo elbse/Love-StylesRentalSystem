d65d7c91d2a83df890405abba52c4e0f24f51a2d
elbse <changpriego@gmail.com>
2026-03-06 22:29:33 +0800

    minor edit

--- resources/views/inventories/index.blade.php
+++ resources/views/inventories/index.blade.php
@@ -112,15 +112,8 @@
     <div class="bg-white rounded-lg shadow-md p-4">
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-3">
             <x-item-card />
-            <x-item-card />
-            <x-item-card />
-            <x-item-card />
-            <x-item-card />
-            <x-item-card />
-            <x-item-card />
-            <x-item-card />
-            <x-item-card />
-            <x-item-card />
+            
+            
         </div>
     </div>
 
