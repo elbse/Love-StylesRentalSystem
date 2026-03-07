45547fddd89d4557cc580d7cabd145120148554f
elbse <changpriego@gmail.com>
2026-03-07 22:29:55 +0800

    inventory update

--- resources/views/inventories/index.blade.php
+++ resources/views/inventories/index.blade.php
@@ -1,20 +1,20 @@
 <x-layout :title="$title">
 
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-6">
-            <x-kpi-card icon="fas fa-users" color="bg-black" symbol="{{ asset('storage/images/vector_peso.png') }}"  background="bg-white">
-                    <h3 class="text-sm text-black">Total Customers</h3>
+            <x-kpi-card icon="fas fa-users" color="bg-black" background="bg-white">
+                    <h3 class="text-sm text-black">Available</h3>
                 </x-kpi-card>
 
-            <x-kpi-card icon="fas fa-users" color="bg-black" symbol="{{ asset('storage/images/vector_peso.png') }}"  background="bg-white">
-                    <h3 class="text-sm text-black">Total Customers</h3>
+            <x-kpi-card icon="fas fa-users" color="bg-black" background="bg-white">
+                    <h3 class="text-sm text-black">Rented Out</h3>
                 </x-kpi-card>
 
-            <x-kpi-card icon="fas fa-users" color="bg-black" symbol="{{ asset('storage/images/vector_peso.png') }}"  background="bg-white">
-                    <h3 class="text-sm text-black">Total Customers</h3>
+            <x-kpi-card icon="fas fa-users" color="bg-black" background="bg-white">
+                    <h3 class="text-sm text-black">Reserved</h3>
                 </x-kpi-card>
 
-            <x-kpi-card icon="fas fa-users" color="bg-black" symbol="{{ asset('storage/images/vector_peso.png') }}"  background="bg-white">
-                    <h3 class="text-sm text-black">Total Customers</h3>
+            <x-kpi-card icon="fas fa-users" color="bg-black" background="bg-white">
+                    <h3 class="text-sm text-black">Total Items</h3>
                 </x-kpi-card>
     </div>
 
